c39f2a3893b7d98731cf4056246a2e73d082a372
Bui Tan An <itgiup.com@gmail.com>
2022-07-18 14:46:25 +0700

    first

---
+++ client/build/precache-manifest.7db370eef348a05f3fe0cfbc8afbd182.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7d51f79b06d39b619ad6e9c1745467a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1950d3978c29da902a31",
+    "url": "/static/css/2.10bddc8f.chunk.css"
+  },
+  {
+    "revision": "2ac4fbee48008e52526b",
+    "url": "/static/css/main.cfda8877.chunk.css"
+  },
+  {
+    "revision": "1950d3978c29da902a31",
+    "url": "/static/js/2.f51db10a.chunk.js"
+  },
+  {
+    "revision": "2ac4fbee48008e52526b",
+    "url": "/static/js/main.35c43708.chunk.js"
+  },
+  {
+    "revision": "0a31ddf7382e0363a369",
+    "url": "/static/js/runtime-main.b240f91f.js"
+  },
+  {
+    "revision": "c06f3a3e804ebc7343949fdca3fdd7f8",
+    "url": "/static/media/metamask.c06f3a3e.svg"
+  }
+]);
